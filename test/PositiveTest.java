65d9c45d5d929a06b4a253f330a3662962a7cf5d
Michael Kolawole <47972946+Mikeskay@users.noreply.github.com>
2022-03-23 16:51:16 +0000

    Create PositiveTest.java

---
+++ test/PositiveTest.java
@@ -0,0 +1,19 @@
+package positive.test;
+
+import positive.base.Positive;
+
+public class PositiveTest {
+
+
+    public static void main(String[] args) {
+
+        Positive test = new Positive();
+
+        test.printDifference();
+
+      
+        
+    }
+
+
+}
